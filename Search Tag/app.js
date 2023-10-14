7826bafab6a3f706e4bb0b05f9b21b6d6d22e3bf
Trần Phúc Nguyên <83873824+tranphucnguyen@users.noreply.github.com>
2023-10-14 16:27:18 +0700

    Add files via upload

---
+++ Search Tag/app.js
@@ -0,0 +1,39 @@
+var content=document.querySelector('.content')
+var input=document.querySelector('.content input')
+var btnRemoveAll=document.querySelector('.remove-all')
+var tags=['Nodejs', 'Reactjs']
+function render(){
+    content.innerHTML=''
+
+    for(let i=0;i<tags.length;i++){
+        const tag=tags[i];
+        content.innerHTML+= `<li>
+                                ${tag}
+                                <i class="fas fa-times" onclick="removeTag(${i})"></i>
+                            </li>
+        ` 
+    }
+    content.appendChild(input)
+    input.focus()
+}
+function removeTag(index){
+    tags.splice(index,1)
+    render()
+}
+
+render()
+
+
+input.addEventListener('keydown',function(event){
+    if(event.key =='Enter'){
+        console.log();
+        tags.push(input.value.trim())
+        input.value=''
+        render()
+    }
+})
+
+btnRemoveAll.addEventListener('click',function(){
+    tags=[]
+    render()
+})
